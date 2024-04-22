2bfa287e35d693f9c7bdff4a3a53f19d97d26ef9
AlejoChavezT <alejo.chavez@alumnos.frgp.utn.edu.ar>
2024-04-22 18:46:08 -0300

    Ventana de Detalle de Articulos

--- Actividad2/Program.cs
+++ Actividad2/Program.cs
@@ -16,7 +16,7 @@ namespace Actividad2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMenuPrincipal());
+            Application.Run(new FormDetalleArticulo());
         }
     }
 }
